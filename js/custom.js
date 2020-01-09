58a1fb5c2c3c65cf97e473dcbc8a2433694f9001
Aaron Lipman <alipman88@gmail.com>
2020-01-09 10:35:08 -0500

    customize layout, hide unnecessary fields

---
+++ js/custom.js
@@ -0,0 +1,15 @@
+function getUrlParameter(name) {
+  name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
+  var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
+  var results = regex.exec(location.search);
+  return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
+};
+
+if (!String.prototype.startsWith) {
+  Object.defineProperty(String.prototype, 'startsWith', {
+    value: function(search, rawPos) {
+      var pos = rawPos > 0 ? rawPos|0 : 0;
+      return this.substring(pos, pos + search.length) === search;
+    }
+  });
+}
